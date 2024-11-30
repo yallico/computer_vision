bea22551b97666dddb374e7e0c3d0916f7a42f4c
Luis Yallico <yallico92@gmail.com>
2024-11-30 11:47:41 +0000

    feature detection draft

--- Scripts/functions.py
+++ Scripts/functions.py
@@ -12,13 +12,64 @@ def preprocess_image(image_path):
     gray_image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
     
     #step 2: histogram equalization using CLAHE
-    clahe = cv2.createCLAHE(clipLimit=2.0, tileGridSize=(8, 8))
+    clahe = cv2.createCLAHE(clipLimit=3, tileGridSize=(12, 12))
     equalized_image = clahe.apply(gray_image)
     
     #step 3: gaussian blur
-    blurred_image = cv2.GaussianBlur(equalized_image, (5, 5), 0)
+    blurred_image = cv2.GaussianBlur(equalized_image, (3, 3), 0.3)
 
     #TODO: currently all hyper parameters are used as default, it required fine-tunning
 
     return blurred_image
 
+def process_and_save_images(image_paths, save_dir):
+    for img_path in image_paths:
+        processed_image = preprocess_image(img_path)
+        if processed_image is not None:
+            #save path
+            img_name = os.path.basename(img_path)
+            save_path = os.path.join(save_dir, img_name)
+            cv2.imwrite(save_path, processed_image)
+        else:
+            print(f"Skipping image: {img_path}")
+
+def extract_and_filter_features(image_path, save_visualization=False, save_dir=None, min_size=5, max_size=50):
+    #load
+    image = cv2.imread(image_path, cv2.IMREAD_GRAYSCALE)
+    
+    if image is None:
+        print(f"Error loading image: {image_path}")
+        return [], None
+
+    #SIFT detector, need to use in grid-search
+    sift = cv2.SIFT_create(
+        nfeatures=0,             #number of best features to retain (0 means no limit)
+        nOctaveLayers=3,         #number of layers in each octave
+        contrastThreshold=0.04,  #threshold for filtering out weak features
+        edgeThreshold=10         #threshold for edge detection
+    )
+    
+    #detect
+    keypoints, descriptors = sift.detectAndCompute(image, mask=None)
+    
+    #filter features by size
+    filtered_keypoints = [kp for kp in keypoints if min_size <= kp.size <= max_size]
+    
+    #update descriptors
+    if descriptors is not None:
+        indices = [i for i, kp in enumerate(keypoints) if kp in filtered_keypoints]
+        filtered_descriptors = descriptors[indices, :]
+    else:
+        filtered_descriptors = None
+    
+    #save sample image with keypoints
+    if save_visualization and save_dir:
+        os.makedirs(save_dir, exist_ok=True)
+        keypoint_image = cv2.drawKeypoints(
+            image, filtered_keypoints, None, flags=cv2.DRAW_MATCHES_FLAGS_DRAW_RICH_KEYPOINTS
+        )
+        img_name = os.path.basename(image_path)
+        save_path = os.path.join(save_dir, img_name)
+        cv2.imwrite(save_path, keypoint_image)
+    
+    return filtered_keypoints, filtered_descriptors
--- Scripts/machine_vision_classic.py
+++ Scripts/machine_vision_classic.py
@@ -1,18 +1,45 @@
 import os
 import random
+from functions import *
+from tqdm import tqdm
 
 image_dir = 'data-collection/images'
 all_images = [os.path.join(image_dir, img) for img in os.listdir(image_dir) if img.endswith('.png')]
 
-# seed shuffle for repeatability
+#seed shuffle for repeatability
 random.seed(42)
 random.shuffle(all_images)
-
-# do 70/30 split
+#do 70/30 split
 split_index = int(len(all_images) * 0.7)
-
-# Split the dataset
+#split the dataset
 train_images = all_images[:split_index]
 test_images = all_images[split_index:]
+
+#set directories for pre-processed images
+train_dir = 'pre-processed/train_images'
+test_dir = 'pre-processed/test_images'
+os.makedirs(train_dir, exist_ok=True)
+os.makedirs(test_dir, exist_ok=True)
+
+#pre-process images
+process_and_save_images(train_images, train_dir)
+process_and_save_images(test_images, test_dir)
+
+#extract features
+image_paths = [os.path.join(train_dir, img) for img in os.listdir(train_dir) if img.endswith('.png')]
+num_samples = 10  #sample for validation
+sample_images = random.sample(image_paths, num_samples)
+vis_dir_feature = 'sample-images-features/'
+all_keypoints = []
+all_descriptors = []
+
+for img_path in tqdm(image_paths):
+    save_vis = img_path in sample_images
+    keypoints, descriptors = extract_and_filter_features(
+        img_path, save_visualization=save_vis, save_dir=vis_dir_feature
+    )
+    all_keypoints.append(keypoints)
+    all_descriptors.append(descriptors)
+
 
 print("Done")
